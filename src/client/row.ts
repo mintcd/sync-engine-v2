ae09df61feae317a35954472a884478b09859ddc
Đăng Minh <112609817+mintcd@users.noreply.github.com>
2026-07-18 23:44:22 +0200

    clarify row runtime exports

--- src/client/row.ts
+++ src/client/row.ts
@@ -1,4 +1,5 @@
 // Public row-runtime surface. Implementations are split by responsibility below.
+export * from "./row/codec";
 export * from "./row/interpreters";
 export * from "./row/operations";
 export * from "./row/schema";
